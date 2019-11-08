c05bb2badc0677a73114fd1f3a21881be0634801
Zong Jie <tzongjie@live.com>
2019-11-08 22:46:27 +0800

    Complete sripts

---
+++ data-collection-pipeline/seedTrafficData.ts
@@ -0,0 +1,430 @@
+export const seedTrafficData = {
+    "blogs": [
+        {
+            "blogUrl": "www.loveandroad.com",
+            "blogTraffic": {
+                "historicalData": [
+                    {
+                        "date": "2019-08-01",
+                        "pageViews": {
+                            "perMillion": 0.03,
+                            "perUser": 1.0
+                        },
+                        "rank": 533258,
+                        "reach": {
+                            "perMillion": 1
+                        }
+                    },
+                    {
+                        "date": "2019-08-02",
+                        "pageViews": {
+                            "perMillion": 0.3,
+                            "perUser": 3.0
+                        },
+                        "rank": 186238,
+                        "reach": {
+                            "perMillion": 4
+                        }
+                    },
+                    {
+                        "date": "2019-08-03",
+                        "pageViews": {
+                            "perMillion": 0.05,
+                            "perUser": 1.0
+                        },
+                        "rank": 375525,
+                        "reach": {
+                            "perMillion": 2
+                        }
+                    },
+                    {
+                        "date": "2019-08-04",
+                        "pageViews": {
+                            "perMillion": 0.03,
+                            "perUser": 1.0
+                        },
+                        "rank": 428596,
+                        "reach": {
+                            "perMillion": 1
+                        }
+                    },
+                    {
+                        "date": "2019-08-05",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 3.0
+                        },
+                        "rank": 277678,
+                        "reach": {
+                            "perMillion": 3
+                        }
+                    },
+                    {
+                        "date": "2019-08-06",
+                        "pageViews": {
+                            "perMillion": 0.09,
+                            "perUser": 1.0
+                        },
+                        "rank": 288640,
+                        "reach": {
+                            "perMillion": 4
+                        }
+                    },
+                    {
+                        "date": "2019-08-07",
+                        "pageViews": {
+                            "perMillion": 0.03,
+                            "perUser": 1.0
+                        },
+                        "rank": 543572,
+                        "reach": {
+                            "perMillion": 1
+                        }
+                    },
+                    {
+                        "date": "2019-08-09",
+                        "pageViews": {
+                            "perMillion": 0.01,
+                            "perUser": 1.0
+                        },
+                        "rank": 728905,
+                        "reach": {
+                            "perMillion": 0.4
+                        }
+                    },
+                    {
+                        "date": "2019-08-10",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 1.0
+                        },
+                        "rank": 176409,
+                        "reach": {
+                            "perMillion": 5
+                        }
+                    },
+                    {
+                        "date": "2019-08-12",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 1.0
+                        },
+                        "rank": 168095,
+                        "reach": {
+                            "perMillion": 7
+                        }
+                    },
+                    {
+                        "date": "2019-08-14",
+                        "pageViews": {
+                            "perMillion": 0.009,
+                            "perUser": 1.0
+                        },
+                        "rank": 818296,
+                        "reach": {
+                            "perMillion": 0.4
+                        }
+                    },
+                    {
+                        "date": "2019-08-15",
+                        "pageViews": {
+                            "perMillion": 0.09,
+                            "perUser": 1.0
+                        },
+                        "rank": 309102,
+                        "reach": {
+                            "perMillion": 3
+                        }
+                    },
+                    {
+                        "date": "2019-08-18",
+                        "pageViews": {
+                            "perMillion": 0.3,
+                            "perUser": 1.0
+                        },
+                        "rank": 103445,
+                        "reach": {
+                            "perMillion": 10
+                        }
+                    },
+                    {
+                        "date": "2019-08-19",
+                        "pageViews": {
+                            "perMillion": 0.1,
+                            "perUser": 2.0
+                        },
+                        "rank": 280294,
+                        "reach": {
+                            "perMillion": 3
+                        }
+                    },
+                    {
+                        "date": "2019-08-20",
+                        "pageViews": {
+                            "perMillion": 0.5,
+                            "perUser": 1.0
+                        },
+                        "rank": 74107,
+                        "reach": {
+                            "perMillion": 10
+                        }
+                    },
+                    {
+                        "date": "2019-08-21",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 1.0
+                        },
+                        "rank": 150270,
+                        "reach": {
+                            "perMillion": 7
+                        }
+                    },
+                    {
+                        "date": "2019-08-22",
+                        "pageViews": {
+                            "perMillion": 0.02,
+                            "perUser": 1.0
+                        },
+                        "rank": 635402,
+                        "reach": {
+                            "perMillion": 0.9
+                        }
+                    },
+                    {
+                        "date": "2019-08-23",
+                        "pageViews": {
+                            "perMillion": 0.02,
+                            "perUser": 1.0
+                        },
+                        "rank": 677817,
+                        "reach": {
+                            "perMillion": 0.6
+                        }
+                    },
+                    {
+                        "date": "2019-08-25",
+                        "pageViews": {
+                            "perMillion": 0.3,
+                            "perUser": 2.0
+                        },
+                        "rank": 134511,
+                        "reach": {
+                            "perMillion": 6
+                        }
+                    },
+                    {
+                        "date": "2019-08-26",
+                        "pageViews": {
+                            "perMillion": 0.1,
+                            "perUser": 1.0
+                        },
+                        "rank": 209025,
+                        "reach": {
+                            "perMillion": 5
+                        }
+                    },
+                    {
+                        "date": "2019-08-27",
+                        "pageViews": {
+                            "perMillion": 0.07,
+                            "perUser": 7.0
+                        },
+                        "rank": 697860,
+                        "reach": {
+                            "perMillion": 0.4
+                        }
+                    },
+                    {
+                        "date": "2019-08-28",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 2.0
+                        },
+                        "rank": 161090,
+                        "reach": {
+                            "perMillion": 6
+                        }
+                    },
+                    {
+                        "date": "2019-08-29",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 3.0
+                        },
+                        "rank": 238665,
+                        "reach": {
+                            "perMillion": 3
+                        }
+                    },
+                    {
+                        "date": "2019-08-30",
+                        "pageViews": {
+                            "perMillion": 0.2,
+                            "perUser": 1.0
+                        },
+                        "rank": 149241,
+                        "reach": {
+                            "perMillion": 7
+                        }
+                    },
+                    {
+                        "date": "2019-08-31",
+                        "pageViews": {
+                            "perMillion": 0.08,
+                            "perUser": 2.0
+                        },
+                        "rank": 325964,
+                        "reach": {
+                            "perMillion": 1
+                        }
+                    }
+                ],
+                "usageData": [
+                    {
+                        "timeRange": {
+                            "period": "Months",
+                            "count": 3
+                        },
+                        "rank": {
+                            "value": 244146,
+                            "delta": -73911
+                        },
+                        "reach": {
+                            "rank": {
+                                "value": 205440,
+                                "delta": -71354
+                            },
+                            "perMillion": {
+                                "value": 2.8,
+                                "delta": "+50%"
+                            }
+                        },
+                        "pageViews": {
+                            "perMillion": {
+                                "value": 0.1,
+                                "delta": "+40%"
+                            },
+                            "rank": {
+                                "value": 385141,
+                                "delta": -88508
+                            },
+                            "perUser": {
+                                "value": 1.3,
+                                "delta": "-2.92%"
+                            }
+                        }
+                    },
+                    {
+                        "timeRange": {
+                            "period": "Months",
+                            "count" : 1
+                        },
+                        "rank": {
+                            "value": 202341,
+                            "delta": -33943
+                        },
+                        "reach": {
+                            "rank": {
+                                "value": 175673,
+                                "delta": -19977
+                            },
+                            "perMillion": {
+                                "value": 3.5,
+                                "delta": "+20%"
+                            }
+                        },
+                        "pageViews": {
+                            "perMillion": {
+                                "value": 0.14,
+                                "delta": "+40%"
+                            },
+                            "rank": {
+                                "value": 311706,
+                                "delta": -73644
+                            },
+                            "perUser": {
+                                "value": 1.5,
+                                "delta": "+20%"
+                            }
+                        }
+                    },
+                    {
+                        "timeRange": {
+                            "period": "Days",
+                            "count": 7
+                        },
+                        "rank": {
+                            "value": 182043,
+                            "delta": -8714
+                        },
+                        "reach": {
+                            "rank": {
+                                "value": 175503,
+                                "delta": 7248
+                            },
+                            "perMillion": {
+                                "value": 4,
+                                "delta": "-4.58%"
+                            }
+                        },
+                        "pageViews": {
+                            "perMillion": {
+                                "value": 0.21,
+                                "delta": "+30%"
+                            },
+                            "rank": {
+                                "value": 241386,
+                                "delta": -44418
+                            },
+                            "perUser": {
+                                "value": 2,
+                                "delta": "+30%"
+                            }
+                        }
+                    },
+                    {
+                        "timeRange": {
+                            "period": "Days",
+                            "count": 1
+                        },
+                        "rank": {
+                            "value": 452198,
+                            "delta": 242698
+                        },
+                        "reach": {
+                            "rank": {
+                                "value": 399914,
+                                "delta": 232656
+                            },
+                            "perMillion": {
+                                "value": 2,
+                                "delta": "-65.312%"
+                            }
+                        },
+                        "pageViews": {
+                            "perMillion": {
+                                "value": 0.06,
+                                "delta": "-57.829%"
+                            },
+                            "rank": {
+                                "value": 501604,
+                                "delta": 174155
+                            },
+                            "perUser": {
+                                "value": 1,
+                                "delta": "+20%"
+                            }
+                        }
+                    }
+                ],
+                "contentData": {
+                    "speed": {
+                        "medianLoadTime": 2266,
+                        "percentile": 37
+                    }
+                }
+            }
+        }
+    ]
+}
